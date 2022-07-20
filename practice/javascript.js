fc91cb2a34465b6a28fe0468acf1a93520995a39
Chiku Elue <chikugreen21@gmail.com>
2022-07-20 11:53:31 -0500

    javascript and css

---
+++ practice/javascript.js
@@ -0,0 +1,84 @@
+// This is how to comment SINGLE LINE ONLY
+/* 
+This is a multi-line comment
+*/
+var myName;
+myName = Chiku;
+
+var a;
+a = 47
+//console.log(a);
+//console.log("this is a")
+
+var b;
+b = a;
+console.log("b is equal to: " + b);
+
+//assignment operator
+var c = 0;
+
+//string
+var myFristName = "Chiku";
+var myLastName = "None of your business";
+var num = "12";
+
+var number = "code";
+
+var x = 1;
+var y = 5;
+var z = "I am a";
+
+x = x + 1;
+y = y + 5;
+z = z + "Coder";
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+var camper = "Chiku";
+var camper = "Wendu";
+console.log(camper);
+
+let student = "Chiku";
+//let student = "Chiku";
+//with let, the variable cannot be overriden or changed.
+console.log(student);
+
+const Fav_Pet = "Dogs";
+
+const SUM = 10 + 10;
+const DIFFERENCE = 45 - 20;
+const QUOTIENT = 66 / 33;
+const PRODUCT = 8 * 10;
+console.log(SUM);
+console.log(DIFFERENCE);
+console.log(QUOTIENT);
+console.log(PRODUCT);
+
+let newVar = 47;
+newVar = newVar + 1;
+//equivalent to...
+newVar++;
+
+console.log(newVar);
+
+newVar = newVar - 1;
+//same as...
+newVar--;
+
+ let v = 1;
+ v += 5;
+ console.log("this is v" + v);
+
+ let s = 10;
+ s -= 5;
+ console.log("this is s" + s);
+
+ let m = 5;
+ s *= 2;
+ console.log("this is m" + m);
+
+ let d = 10;
+ s /= 2;
+ console.log("this is d" + d);
